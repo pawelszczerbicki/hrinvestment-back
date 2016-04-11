8c216d6f3821682f16692d02c5711278af1f6362
shark <szczerbicki.pawel@gmail.com>
2016-04-12 00:20:31 +0200

    return permissions to front

--- src/main/java/pl/hrinvestment/auth/AuthController.java
+++ src/main/java/pl/hrinvestment/auth/AuthController.java
@@ -3,8 +3,10 @@ package pl.hrinvestment.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.hrinvestment.user.Permission;
 import pl.hrinvestment.user.User;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -16,5 +18,10 @@ public class AuthController {
     @RequestMapping(value = "/login", method = POST)
     public User login() {
         return securityService.currentUser().get();
+    }
+
+    @RequestMapping(value = "/permissions", method = GET)
+    public Permission[] permissions() {
+        return Permission.values();
     }
 }
